6a58e895f26b306d1a4c61e7e00335686de2dfc8
Daniel Pinfold <daniel@gilbertpinfold.co.nz>
2018-09-25 23:00:47 +1200

    Untracked files issue resolved to fix .gitignore

--- build/vendor/glm/test/core/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/daniel/workspace/cgra350/cgra-350-assignment-3/work/vendor/glm/test/core
-# Build directory: C:/Users/daniel/workspace/cgra350/cgra-350-assignment-3/build/vendor/glm/test/core
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/vendor/glm/test/gtc/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/daniel/workspace/cgra350/cgra-350-assignment-3/work/vendor/glm/test/gtc
-# Build directory: C:/Users/daniel/workspace/cgra350/cgra-350-assignment-3/build/vendor/glm/test/gtc
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
