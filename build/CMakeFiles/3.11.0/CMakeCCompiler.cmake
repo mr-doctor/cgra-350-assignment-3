d4d685f7224e106230900f2ce02150940beb5ea6
Daniel Pinfold <daniel@gilbertpinfold.co.nz>
2018-09-25 22:59:08 +1200

    Run

--- build/CMakeFiles/3.11.0/CMakeCCompiler.cmake
+++ build/CMakeFiles/3.11.0/CMakeCCompiler.cmake
@@ -1,4 +1,4 @@
-set(CMAKE_C_COMPILER "C:/Program Files (x86)/mingw-w64/i686-7.3.0-posix-dwarf-rt_v5-rev0/mingw32/bin/gcc.exe")
+set(CMAKE_C_COMPILER "C:/Program Files/mingw64/mingw32/bin/gcc.exe")
 set(CMAKE_C_COMPILER_ARG1 "")
 set(CMAKE_C_COMPILER_ID "GNU")
 set(CMAKE_C_COMPILER_VERSION "7.3.0")
@@ -16,11 +16,11 @@ set(CMAKE_C_SIMULATE_VERSION "")
 
 
 
-set(CMAKE_AR "C:/Program Files (x86)/mingw-w64/i686-7.3.0-posix-dwarf-rt_v5-rev0/mingw32/bin/ar.exe")
-set(CMAKE_C_COMPILER_AR "C:/Program Files (x86)/mingw-w64/i686-7.3.0-posix-dwarf-rt_v5-rev0/mingw32/bin/gcc-ar.exe")
-set(CMAKE_RANLIB "C:/Program Files (x86)/mingw-w64/i686-7.3.0-posix-dwarf-rt_v5-rev0/mingw32/bin/ranlib.exe")
-set(CMAKE_C_COMPILER_RANLIB "C:/Program Files (x86)/mingw-w64/i686-7.3.0-posix-dwarf-rt_v5-rev0/mingw32/bin/gcc-ranlib.exe")
-set(CMAKE_LINKER "C:/Program Files (x86)/mingw-w64/i686-7.3.0-posix-dwarf-rt_v5-rev0/mingw32/bin/ld.exe")
+set(CMAKE_AR "C:/Program Files/mingw64/mingw32/bin/ar.exe")
+set(CMAKE_C_COMPILER_AR "C:/Program Files/mingw64/mingw32/bin/gcc-ar.exe")
+set(CMAKE_RANLIB "C:/Program Files/mingw64/mingw32/bin/ranlib.exe")
+set(CMAKE_C_COMPILER_RANLIB "C:/Program Files/mingw64/mingw32/bin/gcc-ranlib.exe")
+set(CMAKE_LINKER "C:/Program Files/mingw64/mingw32/bin/ld.exe")
 set(CMAKE_COMPILER_IS_GNUCC 1)
 set(CMAKE_C_COMPILER_LOADED 1)
 set(CMAKE_C_COMPILER_WORKS TRUE)
@@ -69,5 +69,5 @@ endif()
 
 
 set(CMAKE_C_IMPLICIT_LINK_LIBRARIES "mingw32;gcc;moldname;mingwex;pthread;advapi32;shell32;user32;kernel32;iconv;mingw32;gcc;moldname;mingwex")
-set(CMAKE_C_IMPLICIT_LINK_DIRECTORIES "C:/Program Files (x86)/mingw-w64/i686-7.3.0-posix-dwarf-rt_v5-rev0/mingw32/lib/gcc/i686-w64-mingw32/7.3.0;C:/Program Files (x86)/mingw-w64/i686-7.3.0-posix-dwarf-rt_v5-rev0/mingw32/lib/gcc;C:/Program Files (x86)/mingw-w64/i686-7.3.0-posix-dwarf-rt_v5-rev0/mingw32/i686-w64-mingw32/lib;C:/Program Files (x86)/mingw-w64/i686-7.3.0-posix-dwarf-rt_v5-rev0/mingw32/lib")
+set(CMAKE_C_IMPLICIT_LINK_DIRECTORIES "C:/Program Files/mingw64/mingw32/lib/gcc/i686-w64-mingw32/7.3.0;C:/Program Files/mingw64/mingw32/lib/gcc;C:/Program Files/mingw64/mingw32/i686-w64-mingw32/lib;C:/Program Files/mingw64/mingw32/lib")
 set(CMAKE_C_IMPLICIT_LINK_FRAMEWORK_DIRECTORIES "")
